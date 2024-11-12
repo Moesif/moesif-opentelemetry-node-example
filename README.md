512a3712e36a0d3ff51ff59af68e97fc94658fe3
Xingheng Wang <xinghengwang@users.noreply.github.com>
2024-11-11 19:52:10 -0800

    Add more http instrumentation (#1)
    * add http instruementation
    
    * added HttpInstrumentation
    
    * added example for getting headers.
    
    * use case for heaers
    
    * update with commented code for hint for debugging setup
    
    * added exampe for identify user or company

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 ## Example Moesif Open Telemetry with a Nodejs Project
 
-
 ### Set up
-
 - copy `.env-template` to `.env` and put in your Moesif Application ID.
 - `npm install` install dependencies
 - `npm run start-with-otel`
 - and then send API calls via curl or similar tools.
   - `curl http://localhost:6060/todos`
 
+
+### Limitations
+
+- Currently, only API spans are imported into Moesif.
+
+### Troubleshooting
+
+Temporarily switch to console exporter to see what data is exporting.
+
+```
+ traceExporter: new ConsoleSpanExporter()
+```
--- instrumentation-moesif.js
+++ instrumentation-moesif.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const opentelemetry = require("@opentelemetry/sdk-node");
 const {
   getNodeAutoInstrumentations
 } = require("@opentelemetry/auto-instrumentations-node");
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { W3CTraceContextPropagator } = require("@opentelemetry/core");
 const {
   OTLPTraceExporter
 } = require("@opentelemetry/exporter-trace-otlp-proto");
@@ -10,31 +14,67 @@ const {
   OTLPMetricExporter
 } = require("@opentelemetry/exporter-metrics-otlp-proto");
 const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
-const { Resource } = require('@opentelemetry/resources');
+const { Resource } = require("@opentelemetry/resources");
 const {
   ATTR_SERVICE_NAME,
-  ATTR_SERVICE_VERSION,
-} = require('@opentelemetry/semantic-conventions');
-
+  ATTR_SERVICE_VERSION
+} = require("@opentelemetry/semantic-conventions");
 
 const sdk = new opentelemetry.NodeSDK({
   resource: new Resource({
-    [ATTR_SERVICE_NAME]: 'todos-server',
-    [ATTR_SERVICE_VERSION]: '0.1.0',
+    [ATTR_SERVICE_NAME]: "todos-server",
+    [ATTR_SERVICE_VERSION]: "0.1.0"
   }),
+  // toggle to console exporter to debug your setup.
+  // traceExporter: new ConsoleSpanExporter(),
   traceExporter: new OTLPTraceExporter({
-    // optional - default url is http://localhost:4318/v1/traces
     url: `https://api-dev.moesif.net/v1/traces`,
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {}
   }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({
-      // url: "<your-otlp-endpoint>/v1/metrics", // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-      headers: {}, // an optional object containing custom headers to be sent with each request
-      concurrencyLimit: 1 // an optional limit on pending requests
+  // metricReader: new PeriodicExportingMetricReader({
+  //   exporter: new OTLPMetricExporter({
+  //     headers: {}, // an optional object containing custom headers to be sent with each request
+  //     concurrencyLimit: 1 // an optional limit on pending requests
+  //   })
+  // }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new HttpInstrumentation({
+      headersToSpanAttributes: {
+        client: {
+          requestHeaders: [
+            "User-Agent",
+            "Authorization",
+            "X-User-Id",
+            "X-Test-Header",
+            "X-Company-Id"
+          ],
+          responseHeaders: [
+            "Content-Type",
+            "X-User-Id",
+            "X-Test-Header",
+            "X-Company-Id"
+          ]
+        },
+        server: {
+          requestHeaders: [
+            "User-Agent",
+            "Authorization",
+            "X-User-Id",
+            "X-Test-Header",
+            "X-Company-Id"
+          ],
+          responseHeaders: [
+            "Content-Type",
+            "X-User-Id",
+            "X-Test-Header",
+            "X-Company-Id"
+          ]
+        }
+      }
     })
-  }),
-  instrumentations: [getNodeAutoInstrumentations()]
+  ],
+  textMapPropagator: new W3CTraceContextPropagator()
 });
 sdk.start();
